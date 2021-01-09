aa59bedafdf120118e86cf6e54316d87d14742f9
liellnima <jkaltenborn@uos.de>
2021-01-09 20:34:15 +0100

    changing project structure

--- data_handling/data_preprocessing.py
+++ data_handling/data_preprocessing.py
@@ -1,4 +1,6 @@
 # Data Preprocessing is done here
+import data_generators
+
 import numpy as np
 import pandas as pd
 import openpyxl
@@ -8,7 +10,6 @@ import csv
 import time
 from pathlib import Path # for os independent path handling
 from snowmicropyn import Profile
-# TODO tqdm
 
 # Set folder location of smp data (pnt format)
 SMP_LOC = Path("/home/julia/Documents/University/BA/Data/Arctic/")
@@ -17,19 +18,19 @@ T_LOC = Path("/home/julia/Documents/University/BA/Data/Arctic/MOSAiC_ICE_Tempera
 # Set folder name were export files get saved
 EXP_LOC = Path("smp_csv")
 
-# function to get all unlabelled data
-def get_smp_data(smp, target_dir, export=False, overwrite=False):
-    """ Gets unlabelled smp data.
+# function to get all unlabelled data (meaning its converted, exported and put in one csv file)
+def get_smp_data(smp, target_dir, filename="smp_all.csv", export=False, overwrite=False):
+    """ Gets unlabelled smp data. Writes the complete data into one csv.
     Unlabelled data with pnt file extension is fetched and exported to csv files if export=True.
     csv files are subsequently transformed to pandas frame.
 
     Parameters:
     smp (Path): folder location of smp profiles
-    target_dir (Path): folder name where data should get exported or were it has already been exported
+    target_dir (Path): folder name where converted csv files should get exported or were they have already been exported
+    filename (String): Name for file, where all the smp data is written to. File extension must be csv!
     export (Boolean): indicates if data should be exported from pnt to csv. Default is False
     overwrite (Boolean): important during export; indicates if csv file should be overwriting if csv file already exists
-    Returns:
-    pd.DataFrame: complete smp data as pd.DataFrame
+
     """
     # create dir for csv exports
     if not os.path.exists(target_dir):
@@ -48,19 +49,6 @@ def get_smp_data(smp, target_dir, export=False, overwrite=False):
                 Profile.load(file).export_samples(file_name)
 
     print("finished exporting")
-        # # walks through file system and exports pnt files to csv files
-        # for root, dirs, files in os.walk(smp):
-        #     for file in files:
-        #         if file.endswith(".pnt"):
-        #             # create file name for export
-        #             file_name = Path(target_dir, file.split(".")[0] + ".csv")
-        #             # export only if we want to overwrite files or the file does not exist yet
-        #             if overwrite or not file_name.is_file():
-        #                 # export pnt file as csv
-        #                 smp_raw = Profile.load(os.path.join(root, file))
-        #                 smp_raw.export_samples(file_name)
-
-
 
     # check if something has been exported in smp_csv
     if len(os.listdir(target_dir)) == 0:
@@ -75,9 +63,9 @@ def get_smp_data(smp, target_dir, export=False, overwrite=False):
 
     # column names in csv files
     col_names = ["distance", "force", "smp_idx"]
-    # TODO clear csv file name and giver person option to choose other path and file name!
-    # we will write all data in one csv file
-    with open("smp_all.csv", "w+") as smp_all:
+
+    # we will write all data in one csv file. file is cleared automatically if already existant
+    with open(smp_all_filename, "w+") as smp_all:
         # writer for writing rows in our file
         writer = csv.writer(smp_all)
         # yields each matching csv file
@@ -99,20 +87,12 @@ def get_smp_data(smp, target_dir, export=False, overwrite=False):
                      # write the row into the csv file
                      writer.writerow([row["distance"], row["force"], row["smp_idx"]])
 
-    print("finished creating smp dataframe")
-    # TODO is still killed here, make a datagenerator instead!
-    smp_all_df = pd.read_csv(Path("smp_all.csv"), names=col_names)
-    print("we got the dataframe!")
-    # convert default object datatype to numeric
-    #smp_all_df[["distance", "force"]] = smp_all_df[["distance", "force"]].apply(pd.to_numeric)
-
-    return smp_all_df
-
+    print("finished producing united csv file")
 
 # TODO function to get all labelled data
     # labelled data has init file extension
     # convert to csv files
-    # put it in pandas frame
+    # put it in pandas frame (or csv_all)
 
 # function to get temperature data
 def get_temperature(temp):
@@ -125,7 +105,7 @@ def get_temperature(temp):
     temp_file = pd.read_csv(temp, index_col=0)
     return temp_file
 
-# TODO update check_export
+# TODO update check_export, such that it works with a datagenerator instead!!!
 # method to check if all pnt files have found their way into the united smp dataframe
 def check_export(pnt_dir, smp_df, break_imm=True):
     """ Checks if all smp pnt files can be found in a dataframe
@@ -147,16 +127,17 @@ def check_export(pnt_dir, smp_df, break_imm=True):
     # stores whether file was found
     found_all = []
 
-    # walk through pnt_dir and check if each file name is found in the dataframe
-    for root, dirs, files in os.walk(pnt_dir):
-        for file in files:
-            if file.endswith(".pnt"):
-                smp_was_found = any(smp_df.smp_idx == file.split(".")[0])
-                found_all.append(smp_was_found)
-                if break_imm and not smp_was_found:
-                    print("The following file was not found: ", file.split(".")[0])
-                    return False
-
+    # match all files in the dir who end on .pnt recursively
+    match_pnt = smp.as_posix() + "/**/*.pnt"
+    # use generator to reduce memory usage
+    file_generator = glob.iglob(match_pnt, recursive=True)
+    # yields each matching file and exports it
+    for file in file_generator:
+        smp_was_found = any(smp_df.smp_idx == file.split(".")[0]) # DATAGENERATOR
+        found_all.append(smp_was_found)
+        if break_imm and not smp_was_found:
+            print("The following file was not found: ", file.split(".")[0])
+            return False
 
     # if all values in found_all are True, return True
     if all(smp_found == True for smp_found in found_all):
@@ -184,6 +165,7 @@ def main():
     print("Starting to export and/or convert data")
     tmp = get_temperature(temp=T_LOC)
     start = time.time()
+    # datagenerator still missing here
     smp = get_smp_data(smp=SMP_LOC, target_dir=EXP_LOC, export=False, overwrite=False)
     end = time.time()
     print("Elapsed time for export and dataframe creation: ", end-start)
@@ -194,6 +176,8 @@ def main():
     print_test_df(smp)
     print("Finished export, transformation and printing example features of data.")
 
-# use the three functions to create one pandas dataframe
+
+# Middleterm TODO: make a new file to create different data generators  and find out how you can pipeline them
+# Types of datagenerators: smp, temp, labelled and unlabelled, different window sizes
 if __name__ == "__main__":
     main()
