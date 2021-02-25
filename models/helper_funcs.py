3ed6d436edd5553c5e396da5e2544b219bc41ea5
liellnima <jkaltenborn@uos.de>
2021-02-25 01:06:09 +0100

    Added another file for helper functions such as normalization or reverse normalization.

---
+++ models/helper_funcs.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+def normalize(data, features, min, max):
+    """ Normalizes the given features of a dataframe.
+    Parameters:
+        data (pd.DataFrame): the dataframe to normalize
+        features (list or str): list of strings or a single string indicating the feature to normalize
+            If several feature they must share the same min and max
+        min (int): the minimum of the features
+        max (int): the maximum of the features
+    Returns:
+        pd.DataFrame: data with normalized features
+    """
+    data.loc[:, features] = data.loc[:, features].apply(lambda x: (x - min) / (max - min))
+    return data
+
+def reverse_normalize(data, features, min, max):
+    """ Reverses the normalization of the given features of a dataframe.
+    Parameters:
+        data (pd.DataFrame): the dataframe to reverse normalize
+        features (list or str): list of strings or a single string indicating the feature to reverse normalize.
+            If several feature they must share the same min and max.
+        min (int): the minimum of the features used during normalization
+        max (int): the maximum of the features used during normalization
+    Returns:
+        pd.DataFrame: data with reversed features
+    """
+    data.loc[:, features] = data.loc[:, features].apply(lambda x: (x * (max - min)) + min)
+    return data
