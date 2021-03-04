158256f792d991c1ba787b3d5bcf22c6f52b35b8
liellnima <jkaltenborn@uos.de>
2021-03-04 13:23:25 +0100

    Added new file and folder for hyperparameter tuning.

---
+++ tuning/tuning.py
@@ -0,0 +1,17 @@
+# separate between tuning and models and runner (for the moment just put tuning in another module)
+
+# import all the models
+# import the parameters from the tuning_parameters file
+
+# all based on balanced accuracy
+
+# TODO: all scikit learn models must be able to return only the model!
+
+# GridSearch for all the models from scikitlearn
+
+# in theory grid search is also possible for keras models: https://www.kaggle.com/shujunge/gridsearchcv-with-keras
+# model = KerasClassifier(build_fn=create_model)
+
+# for loops for anns
+
+# for loops for custom models
