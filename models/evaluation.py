d2583a6fcf90ff1e53d15cf4dcb08c2c3aeb32f8
liellnima <jkaltenborn@uos.de>
2021-03-25 00:41:33 +0100

    made saving of plots and metrics possible. added tqdm.

--- models/evaluation.py
+++ models/evaluation.py
@@ -12,6 +12,7 @@ import time
 import numpy as np
 import pandas as pd
 
+from tqdm import tqdm
 from pathlib import Path
 from tabulate import tabulate
 
@@ -104,7 +105,7 @@ def predicting(model, x_train, y_train, x_test, y_test, smp_idx_train, smp_idx_t
 
     return y_pred, y_pred_prob, fit_time, score_time
 
-def metrics_testing(y_test, y_pred, y_pred_prob, fit_time, score_time, labels_order, annot="test", name="Model"):
+def metrics_testing(y_test, y_pred, y_pred_prob, fit_time, score_time, labels_order, annot="test", name="Model", save_dir=None, printing=False):
     """ Calculates and prints metrics.
     Parameters:
         y_test (pd.Series): Testing target data.
@@ -114,8 +115,11 @@ def metrics_testing(y_test, y_pred, y_pred_prob, fit_time, score_time, labels_or
         fit_time (float): fitting time
         score_time (float): scoring time
         labels_order (list): list where a wished labels order is given.
+        save_dir (str): Default None means metrics are printed but not saved. If
+            str the metrics are saved in this folder but they are not printed.
         annot (str): How the metrics results should be annotated.
         name (str): Name of the Model.
+        printing (bool): If results should be printed
     Returns:
         (dict, dict): tuple of dictionaries. The first one contains the general metrics,
             the second one contains the metrics calculated per metric.
@@ -132,22 +136,43 @@ def metrics_testing(y_test, y_pred, y_pred_prob, fit_time, score_time, labels_or
 
     # calculate metrics per label and confusion matrix
     metrics_per_label = calculate_metrics_per_label(y_test, y_pred, name=name, annot=annot, labels_order=labels_order)
-    # print all metrics beautifully (makes only sense on a larger scale)
-    print("\nScores \n")
-    print(pd.Series(scores))
-    print("\nScores per label for Model {}\n".format(name))
     scores_per_label = {key:value for key, value in metrics_per_label.items() if (key != annot+"_confusion_matrix") & (key != "model")}
     scores_per_label = pd.DataFrame.from_dict(scores_per_label, orient="index", columns=[ANTI_LABELS[i] for i in labels_order])
-    print(tabulate(scores_per_label, headers="keys", tablefmt="psql"))
 
-    return scores, metrics_per_label
+    # print all metrics beautifully (makes only sense on a larger scale)
+    if printing:
+        print("\nScores \n")
+        print(pd.Series(scores))
+        print("\nScores per label for Model {}\n".format(name))
+        print(tabulate(scores_per_label, headers="keys", tablefmt="psql"))
+
+    # for returning later
+    scores_output = pd.DataFrame(columns=scores.keys())
+    scores_output.loc[0] = scores.values()
+
+    if save_dir is not None:
+        # for saving
+        del scores["model"]
+        scores= pd.DataFrame({"Metrics": scores.keys(), "Values": scores.values(), "Model": [name] * len(scores.keys())})
+        # save scores
+        with open(save_dir + "/scores_psql.txt", 'w') as f:
+            f.write(tabulate(scores, headers="keys", tablefmt="psql"))
+        with open(save_dir + "/scores_latex.txt", 'w') as f:
+            f.write(tabulate(scores, headers="keys", tablefmt="latex_raw"))
+        # save scores per label
+        with open(save_dir + "/scores_per_label_psql.txt", 'w') as f:
+            f.write(tabulate(scores_per_label, headers="keys", tablefmt="psql"))
+        with open(save_dir + "/scores_per_label_latex.txt", 'w') as f:
+            f.write(tabulate(scores_per_label, headers="keys", tablefmt="latex_raw"))
+
+    return scores_output, metrics_per_label
 
 def plot_testing(y_pred, y_pred_prob, metrics_per_label, x_test, y_test,
                  smp_idx_test, labels_order, annot="test", name="Model",
                  roc_curve=False, confusion_matrix=False, bog_plot_preds=None,
                  bog_plot_trues=None, one_plot=False, pair_plots=False,
                  only_trues=False, only_preds=False, plot_list=None,
-                 save_folder=None, **kwargs):
+                 save_dir=None, **kwargs):
     """ Plots visualization for predictions and metrics.
     Parameters:
         y_pred (np.array): all predictions of the x_test
@@ -176,28 +201,21 @@ def plot_testing(y_pred, y_pred_prob, metrics_per_label, x_test, y_test,
         plot_list (list): Default None. If not None, this is a list of smp names.
             All wished plots are only produced for the named smp profiles.
             (No strings! Must be floats or numeric inside the list!)
-        save_folder (str): Default None means that the plots are not saved.
+        save_dir (str): Default None means that the plots are not saved.
             If this is a string, the plots will be saved in this folder.
     """
-    # TODO test all of this!
-    if save_folder is not None:
-        save_folder_path = Path(save_folder)
-        # check if dir exists and create it if not
-        if not save_folder_path.is_dir():
-            save_folder_path.mkdir(parents=True, exist_ok=True)
-            # create subdirs as well
-            Path(save_folder_path + "/trues").mkdir(parents=True, exist_ok=True)
-            Path(save_folder_path + "/preds").mkdir(parents=True, exist_ok=True)
-            Path(save_folder_path + "/pairs").mkdir(parents=True, exist_ok=True)
-            Path(save_folder_path + "/onesies").mkdir(parents=True, exist_ok=True)
-
     # print confusion matrix
     if confusion_matrix:
+        print("\tPlotting Confusion Matrix...")
         tags = [ANTI_LABELS[label] for label in labels_order]
-        plot_confusion_matrix(metrics_per_label[annot + "_" + "confusion_matrix"], labels=tags, name=name, save_file=save_folder + "/confusion_matrix.png")
+        plot_confusion_matrix(metrics_per_label[annot + "_" + "confusion_matrix"], labels=tags, name=name, save_file=save_dir + "/confusion_matrix.png")
+        print("\t...done.\n")
+
     # print roc auc curve
     if roc_curve and (y_pred_prob is not None):
-        plot_roc_curve(y_test, y_pred_prob, labels_order, name=name, save_file=save_folder + "/roc_curve.png")
+        print("\tPlotting ROC Curve...")
+        plot_roc_curve(y_test, y_pred_prob, labels_order, name=name, save_file=save_dir + "/roc_curve.png")
+        print("\t...done.\n")
 
     smp_trues = []
     smp_preds = []
@@ -206,8 +224,10 @@ def plot_testing(y_pred, y_pred_prob, metrics_per_label, x_test, y_test,
     else:
         smp_names = plot_list
 
+
     # determine the predicted smp profiles
-    for smp_name in smp_names:
+    print("\tCalculating smp predictions:")
+    for smp_name in tqdm(smp_names):
         smp = pd.DataFrame({"mean_force": x_test["mean_force"], "distance": x_test["distance"], "label": y_test, "smp_idx": smp_idx_test})
         smp = reverse_normalize(smp, "mean_force", min=0, max=45)
         smp = reverse_normalize(smp, "distance", min=0, max=1187)
@@ -221,44 +241,56 @@ def plot_testing(y_pred, y_pred_prob, metrics_per_label, x_test, y_test,
 
     # plot all the true profiles
     if only_trues:
-        for smp_name, smp_true in zip(smp_names, smp_trues):
-            smp_name = int(smp_name)
-            smp_labelled(smp_true, smp_name, title="{} SMP Profile Observed\n".format(smp_name), save_file=save_folder + "/trues/smp_" + smp_name + ".png")
+        print("\tPlotting all true smp profiles:")
+        for smp_name, smp_true in tqdm(zip(smp_names, smp_trues), total=len(smp_names)):
+            smp_name_str = str(int(smp_name))
+            save_file = save_dir + "/trues/smp_" + smp_name_str + ".png" if save_dir is not None else None
+            smp_labelled(smp_true, smp_name, title="{} SMP Profile Observed\n".format(smp_name_str), save_file=save_file)
 
     # plot all the predicted profiles
     if only_preds:
-        for smp_name, smp_pred in zip(smp_names, smp_preds):
-            smp_name = int(smp_name)
-            smp_labelled(smp_pred, smp_name, title="{} SMP Profile Predicted with Model {}\n".format(smp_name, name), save_file=save_folder + "/preds/smp_" + smp_name + ".png")
+        print("\tPlotting all predicted smp profiles:")
+        for smp_name, smp_pred in tqdm(zip(smp_names, smp_preds), total=len(smp_names)):
+            smp_name_str = str(int(smp_name))
+            save_file = save_dir + "/preds/smp_" + smp_name_str + ".png" if save_dir is not None else None
+            smp_labelled(smp_pred, smp_name, title="{} SMP Profile Predicted with {}\n".format(smp_name_str, name), save_file=save_file)
 
     # plot all pairs of true and observed profiles
     if pair_plots:
-        for smp_name, smp_true, smp_pred in zip(smp_names, smp_trues, smp_preds):
-            smp_name = int(smp_name)
-            smp_pair_both(smp_true, smp_pred, smp_name, title="Observed and with {} Predicted SMP Profile {}\n".format(name, smp_name), save_file=save_folder + "/pairs/smp_" + smp_name + ".png")
+        print("\tPlotting all pair plots:")
+        for smp_name, smp_true, smp_pred in tqdm(zip(smp_names, smp_trues, smp_preds), total=len(smp_names)):
+            smp_name_str = str(int(smp_name))
+            save_file = save_dir + "/pairs/smp_" + smp_name_str + ".png" if save_dir is not None else None
+            smp_pair_both(smp_true, smp_pred, smp_name, title="Observed and with {} Predicted SMP Profile {}\n".format(name, smp_name_str), save_file=save_file)
 
     # one plot: observation as bar above everything
     if one_plot:
-        for smp_name, smp_true, smp_pred in zip(smp_names, smp_trues, smp_preds):
-            smp_name = int(smp_name)
-            smp_pair(smp_true, smp_pred, smp_name, title="Observed and with {} Predicted SMP Profile {}\n".format(name, smp_name), save_file=save_folder + "/onesies/smp_" + smp_name + ".png")
+        print("\tPlotting all onesies:")
+        for smp_name, smp_true, smp_pred in tqdm(zip(smp_names, smp_trues, smp_preds), total=len(smp_names)):
+            smp_name_str = str(int(smp_name))
+            save_file = save_dir + "/onesies/smp_" + smp_name_str + ".png" if save_dir is not None else None
+            smp_pair(smp_true, smp_pred, smp_name, title="Observed and with {} Predicted SMP Profile {}\n".format(name, smp_name_str), save_file=save_file)
 
     # put all smps together in one plot
     if bog_plot_trues is not None:
+        print("\tPlotting true bogplots:")
         all_smp_trues = pd.concat(smp_trues)
+        save_file = save_dir + "/bogplot_trues.png" if save_dir is not None else None
         all_in_one_plot(all_smp_trues, show_indices=False, sort=True,
-                        title="All Observed SMP Profiles of the Testing Data", file_name=save_folder + "/bogplot_trues.png")
+                        title="All Observed SMP Profiles of the Testing Data", file_name=save_file)
 
     if bog_plot_preds is not None:
+        print("\tPlotting predicted bogplots:")
         all_smp_preds = pd.concat(smp_preds)
+        save_file = save_dir + "/bogplot_preds.png" if save_dir is not None else None
         all_in_one_plot(all_smp_preds, show_indices=False, sort=True,
-                        title="All SMP Profiles Predicted with {}.".format(name), file_name=save_folder + "/bogplot_preds.png")
+                        title="All SMP Profiles Predicted with {}.".format(name), file_name=save_file)
 
 # TODO save or print loss for ANNs?
 # TODO make saving of plots possible
 def testing(model, x_train, y_train, x_test, y_test, smp_idx_train, smp_idx_test,
             unlabelled_data=None, annot="test", name="Model", labels_order=None,
-            impl_type="scikit", save_folder=None, **plot_and_fit_params):
+            impl_type="scikit", save_dir=None, printing=False, **plot_and_fit_params):
     """ Performs testing on a model. Model is fit on training data and evaluated on testing data. Prediction inclusive.
     Parameters:
         model (model): Model on which .fit and .predict can be called. (or baseline )
@@ -279,6 +311,10 @@ def testing(model, x_train, y_train, x_test, y_test, smp_idx_train, smp_idx_test
             used during prediction and fitting. The following types exists:
             \"scikit\" (most models), \"baseline\", \"keras\" (for all ann models),
             \"semi_manual\" (kmeans, gmm, bmm)
+        save_dir (str): Default None, means that the plots and metrics are shown,
+            but not saved. If str, the plots and metrics are saved there without
+            showing.
+        printing (bool): if metrics for each model should be printed. Default False.
         **plot_and_fit_params: contains:
             **plotting: contains booleans about plotting. And a param for saving
                 the plots and a list to indicate the wished smp profiles for
@@ -293,17 +329,33 @@ def testing(model, x_train, y_train, x_test, y_test, smp_idx_train, smp_idx_test
         labels_order = np.sort(np.unique(y_test))
 
     # make predictions for models. y_pred_prob is None if not possible
+    print("\tCalculating Predictions...")
     y_pred, y_pred_prob, fit_time, score_time = predicting(model, x_train,
                 y_train, x_test, y_test, smp_idx_train, smp_idx_test,
                 unlabelled_data, impl_type, **plot_and_fit_params)
+    print("\t...done.\n")
+
+    # create dirs for metrics and plots
+    if save_dir is not None:
+        save_dir_path = Path.cwd() / save_dir
+        # check if dir exists and create it if not
+        if not save_dir_path.is_dir():
+            save_dir_path.mkdir(parents=True, exist_ok=True)
+            # create subdirs as well
+            Path(save_dir_path / "trues").mkdir(parents=True, exist_ok=True)
+            Path(save_dir_path / "preds").mkdir(parents=True, exist_ok=True)
+            Path(save_dir_path / "pairs").mkdir(parents=True, exist_ok=True)
+            Path(save_dir_path / "onesies").mkdir(parents=True, exist_ok=True)
 
     # print metrics
+    print("\tCalculating Metrics...")
     scores, metrics_per_label = metrics_testing(y_test, y_pred, y_pred_prob,
-                fit_time, score_time, labels_order, annot, name)
+                fit_time, score_time, labels_order, annot=annot, name=name, save_dir=save_dir, printing=printing)
+    print("\t...done.\n")
 
     # plot everything
     plot_testing(y_pred, y_pred_prob, metrics_per_label, x_test, y_test,
-                smp_idx_test, labels_order, annot, name, **plot_and_fit_params)
+                smp_idx_test, labels_order, annot=annot, name=name, save_dir=save_dir, **plot_and_fit_params)
 
     # metrics must be saved from the calling function
     return (scores, metrics_per_label)
