e0f1067f970d4a39bdd8d272b74cce785caf363e
liellnima <jkaltenborn@uos.de>
2021-04-02 00:07:08 +0200

    changed parameters for best values found in hyperparameter tuning

--- tuning/tuning_parameters.py
+++ tuning/tuning_parameters.py
@@ -2,14 +2,14 @@
 BASELINE_PARAMS={}
 
 # num_cluster = [15, 30], find_num_clusters = ["acc", "sil"], with and without tsne
-KMEANS_PARAMS={"num_clusters": 15 , "find_num_clusters": "acc"}
+KMEANS_PARAMS={"num_clusters": 30 , "find_num_clusters": "acc"}
 
 # num_components = [15, 30], cov_type = ["tied, "diag"], find_num_clusters = ["acc", "bic"], with and without tsne
-GMM_PARAMS={"num_components": 15, "find_num_clusters": "acc",
-            "cov_type": "tied"}
+GMM_PARAMS={"num_components": 30, "find_num_clusters": "acc",
+            "cov_type": "diag"}
 
 # num_components = [15, 30], cov_type = ["tied, "diag"], with and without tsne
-BMM_PARAMS={"num_components": 15, "cov_type": "tied"}
+BMM_PARAMS={"num_components": 30, "cov_type": "diag"}
 
 # kernel = ["knn", "rbf"], alpha=[0, 0.2, 0.4]
 LABEL_SPREADING_PARAMS={"kernel": "knn", "alpha": 0.2}
@@ -20,33 +20,34 @@ SELF_TRAINER_PARAMS={"criterion": "threshold", "base_model": None} # ATTENTION: 
 
 # n_estimators= [25, 100, 500], criterion=["entropy", "gini"]
 # max_features=["sqrt", "log2"], max_samples=[0.4, 0.6, 0.8], resample=[False, True]
-RF_PARAMS={"n_estimators": 100, "criterion": "entropy", "max_features": "sqrt",
-           "max_samples": 0.6, "resample": False}
+RF_PARAMS={"n_estimators": 1000, "criterion": "entropy", "max_features": "sqrt",
+           "max_samples": 0.4, "resample": True}
 
 # decision_function_shape=["ovo", "ovr"], gamma=["auto", "scale"], kernel=["rbf", "sigmoid"]
-SVM_PARAMS={"decision_function_shape": "ovr", "gamma": "auto", "kernel": "rbf"}
+SVM_PARAMS={"decision_function_shape": "ovr", "gamma": "scale", "kernel": "rbf"}
 
 # n_neighbors=[10, 20, 50, 100, 1000]
-KNN_PARAMS={"n_neighbors": 20} # weights should be always distance
+KNN_PARAMS={"n_neighbors": 10} # weights should be always distance
 
 # n_estimators=[10, 100, 500], sampling_strategy=["all", "not minority"]
-EASY_ENSEMBLE_PARAMS={"n_estimators": 100, "sampling_strategy": "not minority"}
+EASY_ENSEMBLE_PARAMS={"n_estimators": 1000, "sampling_strategy": "all"}
 
 # rnn size=[50, 100, 150]
 # batch_size=[32, 8], epochs=[100], learning_rate=[0.01, 0.001], dropout=[0, 0.2, 0.5], dense_units=[0, 100]
-LSTM_PARAMS={"batch_size": 32, "epochs": 15, "learning_rate": 0.01,
+LSTM_PARAMS={"batch_size": 8, "epochs": 100, "learning_rate": 0.01,
              "rnn_size": 100, "dense_units": 100, "dropout": 0.2}
 
 # rnn size=[50, 100, 150]
 # batch_size=[32, 8], epochs=[100], learning_rate=[0.01, 0.001], dropout=[0, 0.2, 0.5], dense_units=[0, 100]
-BLSTM_PARAMS={"batch_size": 32, "epochs": 15, "learning_rate": 0.01,
-              "rnn_size": 100, "dense_units": 100, "dropout": 0.2}
+# TODO: maybe run BLSTM also with rnn_size 150 and dropout 0.5 once?
+BLSTM_PARAMS={"batch_size": 32, "epochs": 100, "learning_rate": 0.01,
+              "rnn_size": 50, "dense_units": 100, "dropout": 0.2}
 
 # rnn size=[150]
 # batch_size=[32, 8], epochs=[100], learning_rate=[0.001, 0.0001], dropout=[0, 0.5], dense_units=[0, 100],
 # attention=[False, True], bidirectional=[True, False]
 ENC_DEC_PARAMS={"batch_size": 32, "epochs": 15, "learning_rate": 0.001,
-                "rnn_size": 100, "dense_units": 0, "dropout": 0.2,
+                "rnn_size": 100, "dense_best_units": 0, "dropout": 0.2,
                 "attention": False, "bidirectional": False, "regularize": False}
 
 BEST_PARAMS = {"kmeans": KMEANS_PARAMS, "gmm": GMM_PARAMS, "bmm": BMM_PARAMS,
