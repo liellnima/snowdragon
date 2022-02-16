1bb3039da09f4071693c8416fce75a182780faf7
liellnima <jkaltenborn@uos.de>
2022-02-16 00:22:31 -0500

    added roc auc curve function

--- visualization/plot_results.py
+++ visualization/plot_results.py
@@ -2,12 +2,131 @@ import os
 import math
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-from data_handling.data_parameters import MODEL_COLORS, SNOW_TYPES_SELECTION
+from sklearn.metrics import roc_curve, auc
+
+from data_handling.data_parameters import MODEL_COLORS, SNOW_TYPES_SELECTION, COLORS
 from data_handling.data_parameters import ANTI_LABELS, ANTI_LABELS_LONG, LABELS, MODEL_LABELS
 from models.helper_funcs import load_results
+
+
+def plot_roc_auc(y_trues, y_prob_preds, labels, names, legend=True, file_name=None):
+    """ Plotting ROC curves for all labels of a multiclass classification problem.
+    Inspired from: https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
+    Parameters:
+        y_trues (list<list>): True y values for each model (list of lists)
+        y_prob_preds (list<list>): Predicted probabilities for each model (list of lists)
+        labels (list<list>): Labels for each model (list of lists)
+        names (list<str>): Simple list with model names
+        legend (bool): If the legend should be plot in a separate panel
+        file_name (str): path where the plot should be saved. If None the plot is
+            shown and not stored.
+    """
+    # set up figure
+    plt.rcParams["figure.dpi"] = 500
+    number_rows = int(math.ceil(len(y_trues) / 2))
+    height = number_rows * (6 + 2/3) # 3 -> 20, 1 -> 6.66
+    fig, axs = plt.subplots(nrows=number_rows, ncols=2, sharex=False, sharey=True, figsize=(15, height), dpi=500)
+    # remove /n from ANTI_LABELS_LONG
+    anti_labels_super_long = {key: value.replace("\n", ' ') for key, value in ANTI_LABELS_LONG.items()}
+
+    handels = []
+    legend_labels = []
+
+    # iterate through models
+    for i, ax in enumerate(axs.flat):
+        if i >= len(names):
+            if legend:
+                ax.axis("off")
+                ax.set_title("ROC Curves Legend", fontsize=20)
+                ax.legend(handels, legend_labels, loc="center left", prop={"size": 17}, frameon=True, borderpad=1.5, labelspacing=0.8)
+                break
+            else:
+                break
+        y_true = y_trues[i]
+        y_pred = y_prob_preds[i]
+        label = labels[i]
+        name = names[i]
+
+        # compute rates needed for plotting
+        false_pos_rate, true_pos_rate, roc_auc = compute_auc_rates(y_true, y_pred, label)
+
+        # Plot all ROC curves
+        ax.plot(false_pos_rate["micro"], true_pos_rate["micro"],
+                 label="Micro-average",
+                 color="deeppink", linestyle=':', linewidth=4)
+
+        ax.plot(false_pos_rate["macro"], true_pos_rate["macro"],
+                 label="Macro-average",
+                 color="blueviolet", linestyle=':', linewidth=4)
+
+        colors = [COLORS[l] for l in label]
+        for i, color, l in zip(range(len(label)), colors, label):
+            ax.plot(false_pos_rate[i], true_pos_rate[i], color=color, lw=2,
+                     label="Class {}".format(anti_labels_super_long[l]))
+
+        ax.plot([0, 1], [0, 1], "k--", lw=2)
+        ax.set_title("{0}, Micro-avg area: {1:0.2f}".format(MODEL_LABELS[name], roc_auc["micro"]), fontsize=20)
+        handels, legend_labels = ax.get_legend_handles_labels()
+
+    # shared figure stuff
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    #plt.legend(loc="lower right", prop={'size': 7})
+    fig.text(0.5, 0.05, "False Positive Rate", ha="center", fontsize=25)
+    fig.text(0.07, 0.5, "True Positive Rate", va="center", rotation="vertical", fontsize=25)
+    if file_name is None:
+        plt.show()
+    else:
+        plt.savefig(file_name)
+        plt.close()
+
+
+def compute_auc_rates(y_trues, y_prob_preds, labels):
+    """ Computes necessary statistics for roc auc curve
+    Parameters:
+        y_trues (list): true y values
+        y_prob_preds (list): probabilities of predicted y
+        labels (list): labels used
+    Returns:
+        3-tuple <dict>: three dictionaries with false positive rate,
+            true positive rate and roc auc scores
+    """
+    n_classes = len(labels)
+
+    # Compute ROC curve and ROC area for each class
+    false_pos_rate = dict()
+    true_pos_rate = dict()
+    roc_auc = dict()
+    # calculate the one-hot-encoding for y_trues
+    y_trues_dummies = pd.get_dummies(y_trues, drop_first=False).values
+    for i in range(n_classes):
+        false_pos_rate[i], true_pos_rate[i], _ = roc_curve(y_trues_dummies[:, i], y_prob_preds[:, i])
+        roc_auc[i] = auc(false_pos_rate[i], true_pos_rate[i])
+
+    # Compute micro-average ROC curve and ROC area
+    false_pos_rate["micro"], true_pos_rate["micro"], _ = roc_curve(y_trues_dummies.ravel(), y_prob_preds.ravel())
+    roc_auc["micro"] = auc(false_pos_rate["micro"], true_pos_rate["micro"])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([false_pos_rate[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at these points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, false_pos_rate[i], true_pos_rate[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    false_pos_rate["macro"] = all_fpr
+    true_pos_rate["macro"] = mean_tpr
+    roc_auc["macro"] = auc(false_pos_rate["macro"], true_pos_rate["macro"])
+
+    return false_pos_rate, true_pos_rate, roc_auc
+
 
 def plot_model_comparison(performances, plot_rare=False, file_name="", metric_name="accuracy"):
     """Visualize the model accuracies for all classes
@@ -162,11 +281,13 @@ def prepare_evaluation_data(eval_dir):
             of the different models are stored
 
     Returns:
-        triple: (names, confusion_matrices_list, label_orders_list)
+        n-tuple of lists: (names, confusion_matrices_list, label_orders_list, y_true, y_pred_probs)
     """
     cf_matrices = []
     label_orders = []
     names = []
+    y_trues = []
+    y_pred_probs = []
 
     # get all model subdirs
     model_subdirs = next(os.walk(eval_dir))[1]
@@ -174,8 +295,12 @@ def prepare_evaluation_data(eval_dir):
     for model in model_subdirs:
         metric = load_results(eval_dir+"/"+model+"/eval_raw_data/metrics_per_label.pickle")
         order = load_results(eval_dir+"/"+model+"/eval_raw_data/labels_order.pickle")
+        y_true = load_results(eval_dir+"/"+model+"/eval_raw_data/y_test.pickle")
+        y_pred_prob = load_results(eval_dir+"/"+model+"/eval_raw_data/y_pred_prob.pickle")
+        y_trues.append(y_true)
+        y_pred_probs.append(y_pred_prob)
         cf_matrices.append(metric["eval_confusion_matrix"])
         label_orders.append(order)
         names.append(model)
 
-    return names, cf_matrices, label_orders
+    return names, cf_matrices, label_orders, y_trues, y_pred_probs
