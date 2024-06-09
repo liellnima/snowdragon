7b084a613a596ea1b36b9ca059c46abff8c38ce7
liellnima <julia.kaltenborn@mail.mcgill.ca>
2024-06-09 20:30:16 +0200

    script to run the snowdragon class with configs

---
+++ scripts/run.py
@@ -0,0 +1,66 @@
+import yaml
+import argparse 
+
+from pathlib import Path 
+
+from snowdragon import ROOT_DIR
+from snowdragon.handler import Snowdragon
+
+parser = argparse.ArgumentParser(description="Snowdragon Runner.")
+parser.add_argument("--configs", "-c", default="configs/main_config.yaml", type=str, help="configurations for snowdragon runner")
+
+if __name__ == "__main__":
+    """ Execute main functions.
+    """
+    # read in configs 
+    args = parser.parse_args()
+    configs = None
+    with open(args.configs) as stream:
+        try:
+            configs = yaml.safe_load(stream)
+        except yaml.YAMLError as err:
+            print(err)
+
+    raw_data = Path(configs["raw_data"]["smp"])
+
+    preprocessed_data = ROOT_DIR / configs["processed_data"]["preprocessed_data"]
+    exported_smps_dir = ROOT_DIR / configs["processed_data"]["exported_smp_files"]
+    normalized_npz_file = ROOT_DIR / configs["processed_data"]["normalized_npz_file"]
+    npz_file = ROOT_DIR / configs["processed_data"]["npz_file"]
+    evaluation_dir = ROOT_DIR / configs["processed_data"]["evaluation"]
+
+    random_seed = configs["random_seed"]
+
+    snowdragon = Snowdragon(
+        raw_data = raw_data,
+        exported_smps_dir = exported_smps_dir,
+        smp_npz = npz_file,
+        smp_normalized_npz = normalized_npz_file,
+        preprocess_file = preprocessed_data,
+        random_seed = random_seed,
+        label_configs = configs["configs"]["graintypes"],
+    )
+
+    if configs["run"]["preprocess"]:
+        snowdragon.process(
+            smp_npz = npz_file,
+            smp_normalized_npz = normalized_npz_file,
+            preprocess_file = preprocessed_data,
+            random_seed = random_seed,
+            process_config = configs["configs"]["preprocessing"]
+            )
+
+    if configs["run"]["train"]:
+        snowdragon.train() 
+
+    if configs["run"]["validate"]:
+        snowdragon.validate()
+
+    if configs["run"]["test"]:
+        snowdragon.test() 
+
+    if configs["run"]["predict"]:
+        snowdragon.predict()
+
+    if configs["run"]["tune"]:
+        snowdragon.tune()
